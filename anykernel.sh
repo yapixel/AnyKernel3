1a3e36d7efe0bd375bb5ba021df73c898f3bd2fa
debiansid <9624961+debiansid@users.noreply.github.com>
2026-05-07 22:18:53 +0800

    update

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,12 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Google Kernels
+kernel.string=Midori Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-do.check_boot_version=0
 device.name1=
 device.name2=
 device.name3=
@@ -21,39 +20,56 @@ supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
-
 ### AnyKernel install
 ## boot shell variables
-block=boot
-is_slot_device=auto
-ramdisk_compression=auto
-patch_vbmeta_flag=auto
-no_magisk_check=1
+BLOCK=boot
+IS_SLOT_DEVICE=auto
+RAMDISK_COMPRESSION=auto
+PATCH_VBMETA_FLAG=auto
+NO_MAGISK_CHECK=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
 
-# GKI check
-kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
-case $kernel_version in
-    5.1*) ksu_supported=true ;;
-    6.1*) ksu_supported=true ;;
-    6.6*) ksu_supported=true ;;
-    6.12*) ksu_supported=true ;;
-    *) ksu_supported=false ;;
-esac
+ui_print "Flashing Kernel..."
 
-ui_print " " "  -> Wild Kernels Supported: $ksu_supported"
-$ksu_supported || abort "  -> Non-GKI device, abort."
+# Resolving occasional file system I/O latency issues which may cause binary execution exceptions
+sync
+sleep 0.5
+chmod -R 755 $AKHOME/tools
 
 # boot install
 split_boot
-
-if [ -f "$SPLITIMG/ramdisk.cpio" ]; then
+if [ -f "split_img/ramdisk.cpio" ]; then
     unpack_ramdisk
     write_boot
 else
     flash_boot
 fi
-
-ui_print " "
+## end boot install
+if [ -f "$AKHOME/zram.zip" ]; then
+    MODULE_PATH="$AKHOME/zram.zip"
+    KSUD_PATH="/data/adb/ksud"
+    if [ -f "$KSUD_PATH" ]; then
+        ui_print "Installing zram Module..."
+        /data/adb/ksud module install "$MODULE_PATH"
+        ui_print "Installation Complete!"
+    else
+        ui_print "KSUD Not Found, skipping installation..."
+    fi
+else
+    ui_print "ZRAM module Not Found, skipping ZRAM module installation..."
+fi
+if [ -f "$AKHOME/kpn.zip" ]; then
+    MODULE_PATH="$AKHOME/kpn.zip"
+    KSUD_PATH="/data/adb/ksud"
+    if [ -f "$KSUD_PATH" ]; then
+        ui_print "Installing KP-N Module..."
+        /data/adb/ksud module install "$MODULE_PATH"
+        ui_print "Installation Complete!"
+    else
+        ui_print "KSUD Not Found, skipping installation..."
+    fi
+else
+    ui_print "KP-N module Not Found, skipping KP-N module installation..."
+fi
